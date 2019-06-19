a6cb970d51b60791a54a6e5148cdefe11a006997
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-19 01:17:35 +0000

    Add text for export.up and export.down

--- migrations/20190619005104_create_milestones.js
+++ migrations/20190619005104_create_milestones.js
@@ -1,8 +1,18 @@
-
-exports.up = function(knex, Promise) {
+exports.up = function(knex, Promise) {  
+    return Promise.all([
+      knex.schema.createTable('users', function(table){
+        table.string('username');
+        table.string('password');
+        table.timestamps();
+      })
+    ])
+  };
   
-};
+  exports.down = function(knex, Promise) {  
+    return Promise.all([
+      knex.schema.dropTable('users')
+    ])
+  };
 
-exports.down = function(knex, Promise) {
-  
-};
+
+

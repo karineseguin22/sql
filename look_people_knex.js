17e5c438b396ee0acc57ad85e9b0565c2ad273bc
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-19 00:05:45 +0000

    Create knex for searching Famous People

--- look_people_knex.js
+++ look_people_knex.js
@@ -9,5 +9,19 @@ var knex = require('knex')({
     }
 })
 
-const [, ,fname] = process.argv; 
-console.log(fname); 
+const [, ,fname] = process.argv; //arguments from terminal 
+
+//read function
+function namesFound(rows){
+    console.log(`Found ${rows.length} person(s) by the name ${fname} `);
+    for(i = 0; i < rows.length; i++){
+        console.log(`-${i+1}: ${rows[i].first_name} ${rows[i].last_name}, born ${rows[i].birthdate}`)
+    }
+}
+
+//query to read
+knex('famous_people')
+.select('id','first_name', 'last_name','birthdate')
+.where('first_name', 'Paul')
+.then(res => namesFound(res))
+.finally(() => knex.destroy()); 
